e8ee12420cc1fe6c76f996f13694db403a26db02
Oscar Paesi <oscar.paesi@gmail.com>
2018-06-10 17:44:08 -0300

    Moved the Vue logo and the default style to the HelloWorld component.
    The HelloWorld component will be kept for troubleshooting purposes for now.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="hello">
+  <div id="hello">
+    <img src="../assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -96,6 +97,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+#hello {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
 h1, h2 {
   font-weight: normal;
 }
